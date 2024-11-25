fe9b5640ca9845c6ec9b825fadea69c7a02cf770
Da0vid <david.sanchez478@pascualbravo.edu.co>
2024-11-24 19:55:44 -0500

    final

--- src/Componentes/Proyectos.js
+++ src/Componentes/Proyectos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import P1 from '../Img/Proyecto.png';
+import huellas from '../Img/Huellitas.png'
 
 const Proyecto = () => {
   return (
@@ -8,6 +9,11 @@ const Proyecto = () => {
       <p className="p_vuelos">Este Proyecto lo desarrollé en C# con Windows Forms.<br/> 
       Es una aplicacion de escritorio para una aerolinea en la  <br/>
       cual se puede comprar, ver y cancelar vuelos.
+      </p>
+      <img className="huellitas" src={huellas}/>
+      <p className="p_huellitas">Este Proyecto lo realicé en equipo con otros compañeros.<br/>
+      Consiste en una pagina web desarrollada con ASP.NET, la cual<br/>
+      nos permite gestionar las citas para una peluqueria canina.
       </p>
     </div>
   );
